1a3bdd11f3fe4eeca68b771d7c0113e0943de618
axelcardozo <cardozoaxel@outlook.com>
2021-11-27 23:55:19 -0300

    ola k ase

---
+++ BarreraPAN/mylib.h
@@ -0,0 +1,21 @@
+#ifndef MYLIB
+#define MYLIB
+#include <stdio.h>
+#include <stdbool.h>
+
+typedef enum {
+    libre = 1,
+    ocupado = 0
+}estados_t;
+
+typedef struct {
+  char estado_inicial[8];  // Estado inicial del circuito
+  char configuracion_abierto[8];  // Palabra que representa al circuito abierto en el sensor
+  char configuracion_cerrado[8];  // Palabra que representa al circuito cerrado en el sensor
+}configuracion_circuito_t;
+
+configuracion_circuito_t f_inicio (void);  // Lee el archivo de configuración y carga las variables.
+estados_t f_libre (configuracion_circuito_t);
+estados_t f_ocupado (configuracion_circuito_t);
+
+#endif // MYLIB
